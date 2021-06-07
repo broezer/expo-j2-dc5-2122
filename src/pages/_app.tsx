7dfd437dd4e5195b910b0289938caee43b242e09
broezer <bruce@moerdjiman.com>
2021-06-07 23:58:55 +0200

    remove python scripts, added video

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,7 +44,7 @@ export default function App({ Component, pageProps }) {
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Video src="https://player.vimeo.com/external/494532882.hd.mp4?s=ad7ca70b1861433a0ac257cd5965d72db4fd2daf&profile_id=175" />
+        <Video src="https://player.vimeo.com/external/560157629.hd.mp4?s=bfdc554849be069783f1b9e680e6e2baa575c519&profile_id=175" />
                 
         <Component {...pageProps}  />
         
